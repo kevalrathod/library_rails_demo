286316ad4abf34e7db69b19db3205d3f2eb7e820
Keval Rathod <kevalrathod1111@gmail.com>
2019-03-07 15:26:05 +0530

    adding comment sections in article app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  #authenticate the user with id and password
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles=Article.all
   end
